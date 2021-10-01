98622b35d8930ccdd44b4e10dcf54171960ccc0c
DongGyun Han <me@donggyun.com>
2021-10-01 17:09:10 +0800

    revise deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ npm run build
 aws s3 sync build s3://${DOMAIN}
 
 # Refresh CloudFront
-distirubtion_id=`aws cloudfront list-distributions | jq -r ".DistributionList.Items[]| select(.Aliases | .Items | contains([\"${DOMAIN}\"])) | .Id"`
+distirubtion_id=`aws cloudfront list-distributions | jq -r ".DistributionList.Items[]| select(.Aliases | .Items | index(\"${DOMAIN}\")) | .Id"`
 aws cloudfront create-invalidation --distribution-id $distirubtion_id --paths "/index.html"
