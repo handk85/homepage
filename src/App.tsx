433f634a5edde2359e3893db1598750cdf4a392e
DongGyun Han <me@donggyun.com>
2021-10-05 10:46:47 +0800

    add banner

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
 import Button from "react-bootstrap/Button";
+import Banner from "./Banner";
 
 function Contact() {
   return (
@@ -54,6 +55,7 @@ function CV() {
 function App() {
   return (
     <>
+      <Banner content="I am looking for a faculty position in software engineering" />
       <Abstract />
       <Container>
         <hr />
