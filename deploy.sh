67a1e04305ae844e7dc67b32964d11c87e0c6e7f
DongGyun Han <me@donggyun.com>
2021-12-22 15:36:55 +0900

    add SANER paper

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ aws s3 sync build s3://${DOMAIN} --exclude "icons/*"
 
 # Refresh CloudFront
 distirubtion_id=`aws cloudfront list-distributions | jq -r ".DistributionList.Items[]| select(.Aliases | .Items | index(\"${DOMAIN}\")) | .Id"`
-aws cloudfront create-invalidation --distribution-id $distirubtion_id --paths "/index.html"
+aws cloudfront create-invalidation --distribution-id $distirubtion_id --paths "/index.html" "/data/*"
