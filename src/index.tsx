ef254cc630952bb2d4f84e8ccf00e4530117e491
DongGyun Han <me@donggyun.com>
2022-08-22 14:36:58 +0100

    upgrade bootstrap dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-dark-4/dist/bootstrap-dark.css";
+import "bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
